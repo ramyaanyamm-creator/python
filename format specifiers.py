eea3e548f8a3582f2895b95708014a66ecf2b0a7
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-07-08 19:16:28 +0530

    Add files via upload

---
+++ format specifiers.py
@@ -0,0 +1,10 @@
+print("."*35)
+name="Ramya"
+age=19
+salary=60000
+cgpa=8.09233
+print(f"{'Name':<10} : {name}")
+print(f"{'Age':10} : {age}")
+print(f"{'salary':<10} : {salary:,}")
+print(f"{'CGPA':<10} : {cgpa:.2f}")
+print("."*35)
