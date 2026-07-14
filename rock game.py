d2e7fe7647eb118b1442bfe11c6cf34030f416e2
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-07-14 15:45:47 +0530

    Create rock game.py

---
+++ rock game.py
@@ -0,0 +1,7 @@
+import random
+options=("rock","paper","sessior")
+player=None
+computer=random.choice(options)
+player=input("enter your option:")
+print(f"player:{player}")
+print(f"computer:{computer}")
