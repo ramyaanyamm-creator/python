f287c63a92ef12b18421454a05b78b86426b5fe5
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-07-15 11:37:36 +0530

    function_ex.py

---
+++ function_ex.py
@@ -0,0 +1,9 @@
+#due date
+def invoice(name,amount,due):
+    print(f"Hello this is Ramya is it {name}")
+    print(f"you take a loan ${amount} in my bank")
+    print(f"please pay the amount the due date is {due}")
+    print()
+    
+invoice("Mani",20000,"20/10/2025")
+invoice("Sharon",30000,"1/5/2026")
