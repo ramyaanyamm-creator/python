e952bef424f2ce12d182817c770411485ad647d5
ramyaanyamm-creator <ramyaanyamm@gmail.com>
2026-07-15 11:30:30 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,11 @@
+#functions
+def song(name,years):
+    print(f"happy birthday to you {name}")
+    print(f"it is your {years} birthday")
+    print("happy birthday to you")
+    print("happy birthday to you")
+    print()
+
+song("ramya",19)
+song("sharon",22)
+song("kumar",22)
